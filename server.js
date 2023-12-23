529c2d7b5420ceab325ff9ace7094711724aecbf
Victor Tlaes <94009032+victortlaes@users.noreply.github.com>
2023-12-23 02:49:27 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -9,40 +9,49 @@ const port = 3005;
 app.use(bodyParser.json());
 app.use(cors());
 
+let searchedData = [];
+
 app.get('/', (req, res) => {
   res.send('Servidor está rodando.');
 });
 
-app.get('/games', async (req, res) => {
+app.post('/games', async (req, res) => {
   try {
     const clientId = "ilt59r1k367kepww23p20rn90n0npb";
     const clientSecret = "hj0z6g2ze8o9q1zg6d2818y1vq5p50";
+
+    const { search } = req.body;
 
     // Obtém o token de acesso
     const authUrl = `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`;
     const authResponse = await axios.post(authUrl);
     const token = authResponse.data.access_token;
 
-    // Faz a solicitação à API IGDB com o token de acesso
-    const igdbUrl = "https://api.igdb.com/v4/games?limit=100";
+    // Faz a solicitação à API IGDB com o token de acesso e o termo de pesquisa
+    const igdbUrl = "https://api.igdb.com/v4/games";
     const igdbResponse = await axios.get(igdbUrl, {
       params: {
-      fields:"name, genres.name, cover.url",
-      limit:100,
-      }, 
+        fields: "name, genres.name, cover.*",
+        search: search,
+        limit: 100,
+      },
       headers: {
         'Client-ID': clientId,
         'Authorization': `Bearer ${token}`,
       },
     });
 
-    console.log (igdbResponse.data);
+    searchedData = igdbResponse.data;
 
     res.json(igdbResponse.data);
   } catch (error) {
-    console.error('Erro:', error);
+    console.error('Erro na solicitação à API externa:', error);
     res.status(500).send('Erro interno do servidor');
   }
+});
+
+app.get('/games', (req, res) => {
+  res.json(searchedData);
 });
 
 app.listen(port, () => {
